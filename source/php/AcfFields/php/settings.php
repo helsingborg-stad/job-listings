7cbc18e8eb835aa7635d8babfb994aa8448a0fd9
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-05-24 16:31:34 +0200

    Language

--- source/php/AcfFields/php/settings.php
+++ source/php/AcfFields/php/settings.php
@@ -3,7 +3,7 @@
 if (function_exists('acf_add_local_field_group')) {
     acf_add_local_field_group(array(
     'key' => 'group_5c9de924499d4',
-    'title' => __('Import settings', 'job-listings'),
+    'title' => 'Import settings',
     'fields' => array(
         0 => array(
             'key' => 'field_5c9deb7c5c643',
