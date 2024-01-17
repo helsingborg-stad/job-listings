9d4bc9317f052cff05f7c5f19fe252c3e4134b5c
GitHub Action <>
2024-01-17 07:44:38 +0000

    build: update package file versions to 4.1.2

--- job-listings.php
+++ job-listings.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Job Listings
  * Plugin URI:        (#plugin_url#)
  * Description:       
- * Version: 4.1.1
+ * Version: 4.1.2
  * Author:            Nikolas Ramstedt, Sebastian Thulin & Johan Silvergrund
  * Author URI:        
  * License:           MIT
